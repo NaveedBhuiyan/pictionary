d2eee76a08625139564bf17e87e3cf7cb4942cab
NaveedBhuiyan <naveedbhuiyan21@gmail.com>
2024-07-28 09:40:26 +0200

    added username field. Bugfix prompt to ask for username everytime

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -6,24 +6,32 @@ app.config['SECRET_KEY'] = 'secret!'
 ROOM_PASSWORD = 'lalaland'
 socketio = SocketIO(app)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    session.clear()
     error = None  # Initialize the error variable
     if request.method == 'POST':
-        # Get the password from the form
+        # Get the username and password from the form
+        username = request.form.get('username')
         entered_password = request.form.get('password')
+
         if entered_password == ROOM_PASSWORD:
-            # Password is correct, render the index.html
+            # Password is correct, store the username in the session
+            session['username'] = username
+            # Redirect to the index page
             return redirect(url_for('index'))
         else:
             # Password is incorrect, set an error message
             error = 'Invalid password, please try again.'
+
     # Render the password page with an error message if applicable
     return render_template('password.html', error=error)
 
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    username = session.get('username', 'Guest')  # Retrieve the username from the session
+    return render_template('index.html', username=username)
 
 @socketio.on('draw')
 def handle_draw(data):
